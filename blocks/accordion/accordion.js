67c7017b4f6bdb291a9e486d0effac0092136d09
vineetvn-ust <vineet.vn@ust.com>
2026-08-26 14:13:31 +0530

    fix: js issue

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,27 +1,27 @@
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
-    [...block.children].forEach((row) => {
-        const label = row.children[0];
-        const summary = document.createElement('summary');
-        summary.className = 'accordion-item-label';
+  [...block.children].forEach((row) => {
+    const label = row.children[0];
+    const summary = document.createElement('summary');
+    summary.className = 'accordion-item-label';
 
-        const accordionIcon = document.createElement('img');
-        accordionIcon.className = 'accordion-item-icon';
-        accordionIcon.src = './icons/faq-icon.svg';
-        summary.append(icon);
+    const accordionIcon = document.createElement('img');
+    accordionIcon.className = 'accordion-item-icon';
+    accordionIcon.src = './icons/faq-icon.svg';
+    summary.append(accordionIcon);
 
-        moveInstrumentation(label, summary);
-        summary.append(...label.childNodes);
+    moveInstrumentation(label, summary);
+    summary.append(...label.childNodes);
 
-        const body = row.children[1];
-        body.className = 'accordion-item-body';
+    const body = row.children[1];
+    body.className = 'accordion-item-body';
 
-        const details = document.createElement('details');
-        details.className = 'accordion-item';
-        moveInstrumentation(row, details);
-        details.append(summary, body);
+    const details = document.createElement('details');
+    details.className = 'accordion-item';
+    moveInstrumentation(row, details);
+    details.append(summary, body);
 
-        row.replaceWith(details);
-    });
+    row.replaceWith(details);
+  });
 }
