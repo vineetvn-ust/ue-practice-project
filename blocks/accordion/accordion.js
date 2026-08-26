771cf682376dd13a3f5701d54aa230c90a38f9b4
vineetvn-ust <vineet.vn@ust.com>
2026-08-26 14:03:51 +0530

    feat: add acoordion icon

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,22 +1,27 @@
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
-  [...block.children].forEach((row) => {
-    // decorate accordion item label
-    const label = row.children[0];
-    const summary = document.createElement('summary');
-    summary.className = 'accordion-item-label';
-    moveInstrumentation(label, summary);
-    summary.append(...label.childNodes);
+    [...block.children].forEach((row) => {
+        const label = row.children[0];
+        const summary = document.createElement('summary');
+        summary.className = 'accordion-item-label';
 
-    const body = row.children[1];
-    body.className = 'accordion-item-body';
+        const accordionIcon = document.createElement('img');
+        accordionIcon.className = 'accordion-item-icon';
+        accordionIcon.src = './icons/faq-icon.svg';
+        summary.append(icon);
 
-    const details = document.createElement('details');
-    details.className = 'accordion-item';
-    moveInstrumentation(row, details);
-    details.append(summary, body);
+        moveInstrumentation(label, summary);
+        summary.append(...label.childNodes);
 
-    row.replaceWith(details);
-  });
+        const body = row.children[1];
+        body.className = 'accordion-item-body';
+
+        const details = document.createElement('details');
+        details.className = 'accordion-item';
+        moveInstrumentation(row, details);
+        details.append(summary, body);
+
+        row.replaceWith(details);
+    });
 }
