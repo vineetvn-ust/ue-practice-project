1b471e4fa2299689a4328e70b12d9f9721ddd930
vineetvn-ust <vineet.vn@ust.com>
2026-08-26 12:40:22 +0530

    fix: lint issues

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,19 +1,18 @@
-
 export default function decorate(block) {
-    [...block.children].forEach((row) => {
-        // decorate accordion item label
-        const label = row.children[0];
-        const summary = document.createElement('summary');
-        summary.className = 'accordion-item-label';
-        summary.append(...label.childNodes);
+  [...block.children].forEach((row) => {
+    // decorate accordion item label
+    const label = row.children[0];
+    const summary = document.createElement('summary');
+    summary.className = 'accordion-item-label';
+    summary.append(...label.childNodes);
 
-        const body = row.children[1];
-        body.className = 'accordion-item-body';
+    const body = row.children[1];
+    body.className = 'accordion-item-body';
 
-        const details = document.createElement('details');
-        details.className = 'accordion-item';
-        details.append(summary, body);
+    const details = document.createElement('details');
+    details.className = 'accordion-item';
+    details.append(summary, body);
 
-        row.replaceWith(details);
-    });
+    row.replaceWith(details);
+  });
 }
