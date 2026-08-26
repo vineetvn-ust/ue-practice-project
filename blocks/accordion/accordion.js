f57d1f08b9a6176dfbc106a0dc7646fc17e4c534
vineetvn-ust <vineet.vn@ust.com>
2026-08-26 13:29:18 +0530

    fix: accordion

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,9 +1,12 @@
+import { moveInstrumentation } from '../../scripts/scripts';
+
 export default function decorate(block) {
   [...block.children].forEach((row) => {
     // decorate accordion item label
     const label = row.children[0];
     const summary = document.createElement('summary');
     summary.className = 'accordion-item-label';
+    moveInstrumentation(label, summary);
     summary.append(...label.childNodes);
 
     const body = row.children[1];
@@ -11,6 +14,7 @@ export default function decorate(block) {
 
     const details = document.createElement('details');
     details.className = 'accordion-item';
+    moveInstrumentation(row, details);
     details.append(summary, body);
 
     row.replaceWith(details);
